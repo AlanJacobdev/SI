5d878aa8b38bb2b46293414810801ea7cce8e17e
AlanJacobdev <55379884+AlanJacobdev@users.noreply.github.com>
2020-05-01 19:27:29 +0200

    EX4.3

---
+++ Ex4.3/src/graphicLayer/GContainer.java
@@ -0,0 +1,15 @@
+package graphicLayer;
+
+import java.awt.Point;
+
+public interface GContainer {
+	void repaint();
+
+	Point getGlobalPosition();
+
+	GElement[] getRawContents();
+
+	void addElement(GElement m);
+
+	void removeElement(GElement m);
+}
